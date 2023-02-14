93a24f5092fe52695fec7a04c041f3451eeb007e
Fresco <Cevallosnancy@homail.com>
2023-02-14 12:58:44 -0500

    Trabajo Api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useEffect, useSatate, useState} from 'react'
+import Navbar from './components/Navbar'
+import Characters from './components/Characters';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [characters, setCharacters] = useSatate([]);
+
+  const initiaUrl = "https://rickandmortyapi.com/api/character";
+  
+  const fetchCharacteres = (url) =>{
+      fetch(url)
+      .then(response => response.json())
+      .then(data => setCharacters(data.results))
+      .catch(error => console.log(error))
+    }
+    
+  useEffect(() =>{
+    fetchCharacteres(initiaUrl);
+  }, [])
+
+  return ( 
+    <>
+      <Navbar brand={"Rick and Morty App"} />
+    <div className='container mt-5'>
+      <Characters characters={characters}/>
     </div>
+    </>
   );
 }
 
