3f03131bfc8228c84c678087e72bbe6b3bbe7091
DENIEL1950 <Trejoceballo@gmail.com>
2023-02-14 21:34:04 -0500

    Tarea Api resubido

--- src/components/Datos.js
+++ src/components/Datos.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 export default function Datos() {
   return (
-    <div>
-        Trejo Daniel
-    </div>
+    <div className='container'>
+                <div className='row mt-4'>
+                    <div className='col-md-4'>
+                            <div className='card mt-4'>
+                                <div className='card-header'>
+                                    <h1>Trejo Daniel</h1>
+                                </div>
+
+                                    <div className='card-body'>
+                                        <h5><p>18 Años<hr/>Estudiante de la Pucese sede Esmeraldas</p></h5>
+                                    </div>
+                                    <div className='card-footer'>
+                                        <h5>Ecuatoriano desde la cuna</h5>
+
+                                    </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
   )
 }
